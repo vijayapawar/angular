890f6de6eb9c7191f6fb8deedbe32083b48d01df
Brad Traversy <tectguyinfo@gmail.com>
2018-10-03 13:35:34 +0530

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ServicesComponent,
+    ContactusComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot
+    ([{
+      path :'Home',component:HomeComponent},
+      {path:'AboutUs',component:AboutusComponent},
+      {path:'Contactus',component:ContactusComponent},
+      {path:'Services',component:ServicesComponent},
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
